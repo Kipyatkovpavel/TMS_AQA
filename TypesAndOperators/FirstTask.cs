d878669f602f8f1db6604fa687e992550ae18872
PavelKipyatkov <pykipyatkov@gmail.com>
2023-12-10 19:33:37 +0300

    create new homework branch ArraysAndCycles and FirstTask

---
+++ TypesAndOperators/FirstTask.cs
@@ -0,0 +1,20 @@
+﻿//"Массивы и циклы". Задача 1
+/*Задание 1 Создайте массив целых чисел. Напишете программу, 
+  которая выводит сообщение о том, входит ли заданное число 
+ в массив или нет. Пусть число для поиска задается с консоли.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndOperators;
+
+public static class FirstTask
+{
+    public static void CheckNumberIsInArray()
+    { }
+
+}
