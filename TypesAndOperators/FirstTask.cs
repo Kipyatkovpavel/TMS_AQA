59f14234b6a5dc959a984b1a895c258f5075e777
PavelKipyatkov <pykipyatkov@gmail.com>
2023-12-10 19:17:02 +0300

    add new branck hw

---
+++ TypesAndOperators/FirstTask.cs
@@ -0,0 +1,10 @@
+﻿//"Массивы и циклы". Задача 1
+namespace TypesAndOperators
+{
+    static class FirstTask
+    {
+        static void Main1(string[] args)
+    internal class FirstTask
+    {
+    }
+}
