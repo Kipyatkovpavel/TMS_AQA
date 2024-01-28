219538a04b00d71ccade8a5b683c9e68719b3c33
PavelKipyatkov <pykipyatkov@gmail.com>
2024-01-28 16:33:26 +0300

    new branch OOP and add Abstraction

---
+++ Abstraction/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstraction
+{
+    internal class Client
+    {
+        public int Id;
+        public string PhoneNumber;
+        public string Email;
+
+        public void Activate() { }
+        public void Pause() { }
+        public void Deactivate() { }
+    }
+}
