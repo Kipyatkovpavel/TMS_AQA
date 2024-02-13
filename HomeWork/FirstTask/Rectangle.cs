317de651c8a14717f84a4ef47c9aac3c6ff9c26d
PavelKipyatkov <pykipyatkov@gmail.com>
2024-02-13 23:50:45 +0300

    Finish HW Abstract

---
+++ HomeWork/FirstTask/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.FirstTask
+{
+    public class Rectangle : Figure
+    {
+        public double LengthA;
+        public double LengthB;
+
+        public Rectangle(double lengthA, double lengthB)
+        {
+            this.LengthA = lengthA;
+            this.LengthB = lengthB;
+        }
+        public override double GetPerimeter()
+        {
+            double p = (LengthA+LengthB)* 2;
+            return p;
+        }
+
+        public override double GetArea()
+        {
+            double s = LengthA*LengthB;
+            return s;
+        }
+        public override void GetInfo()
+        {
+            Console.Write("Прямоугольник: ");
+        }
+    }
+}
---
+++ HomeWork/SecondTask/BaseProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.SecondTask
+{
+    public abstract class BaseProduct
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public DateOnly ProductionDate { get; set; } //Дата производства
+        public DateOnly ShelfLife { get; set; } //Срок годности
+
+        public BaseProduct()
+        {
+        }
+        public BaseProduct(string Name)
+        {
+            this.Name = Name;
+        }
+        public BaseProduct(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public BaseProduct(string name, float price, DateOnly productionDate)
+        {
+            Name = name;
+            Price = price;
+            ProductionDate = productionDate;
+
+        }        
+        public BaseProduct(string name, float price, DateOnly productionDate, DateOnly shelfLife)
+        {
+            Name = name;
+            Price = price;
+            ProductionDate = productionDate;
+            ShelfLife = shelfLife;
+        }
+        public abstract void GetProductInfo();
+        public abstract void CheckShelfLife();
+    }
+}
