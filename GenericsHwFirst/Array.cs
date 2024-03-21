2afe893a06f63e571043ea9dc079945062f86809
PavelKipyatkov <pykipyatkov@gmail.com>
2024-03-21 20:48:47 +0300

    add HW Generics

---
+++ GenericsHwFirst/Array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHwFirst
+{
+    public class Array<T>
+    {
+        public T[] array = new T[0];
+
+        public void Add(T value)
+        {
+            List<T> list = new List<T>(array);
+            list.Add(value);
+            array = list.ToArray();
+        }
+        public void Remove(int index)
+        {
+            List<T> list = new List<T>(array);
+            list.RemoveAt(index);
+            array = list.ToArray();
+        }
+        public void PrintArray()
+        {
+            for (int index =  0; index < array.Length; index++) 
+            {
+                Console.WriteLine(array[index]);
+            }
+        }
+        public T GetIndex(int index)
+        {
+            return array[index];
+        }
+        public int GetLenght()
+        {
+            return array.Length;
+        }
+    }
+}
---
+++ GenericsHwFirst/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHwFirst
+{
+    internal class Point<T>
+    {
+        public T x;
+        public T y;
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void PointInfo()
+        {
+            Console.WriteLine($"x: {x}, y: {y}");
+        }
+    }
+}
